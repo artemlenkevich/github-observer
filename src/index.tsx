a43b33303e931986b9e63fe26436269e8e61f02c
Artem Lenkevich <ar.lenkevich@gmail.com>
2021-12-13 20:18:23 +0300

    added token

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,10 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client"
 
 const client = new ApolloClient({
   uri: 'https://api.github.com/graphql',
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
+  headers: {
+    authorization: `Bearer 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`
+  }
 })
 
 ReactDOM.render(
