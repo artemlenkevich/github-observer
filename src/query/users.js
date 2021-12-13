7312ad7afb2a10192ab0fbef887bf6aeb4274eea
Artem Lenkevich <ar.lenkevich@gmail.com>
2021-12-14 00:28:01 +0300

    added spaces

--- src/query/users.js
+++ src/query/users.js
@@ -2,15 +2,15 @@ import {gql} from '@apollo/client'
 
 export const GET_USERS = gql`
     query { 
-        search(query: "artem", type: USER, first: 10) {
+        search(query: "artem", type: USER, first: 5) {
             userCount
             edges {
-            node {
-                ... on User {
-                name
-                login
+                node {
+                    ... on User {
+                    name
+                    login
+                    }
                 }
-            }
             }
         }
     }
